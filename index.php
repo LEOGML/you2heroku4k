54873ab157e9437977e86c070367b13678c9c852
LEOGML <40917063+LEOGML@users.noreply.github.com>
2018-09-01 06:12:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ ini_set('display_errors', '0');
 //把下面的资料改成您的
 
 //网站名字
-$siteName='DouDouTube';
+$siteName='林哥';
 
 //youtube API V3 KEY:
 
-$key='AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c';
+$key='AIzaSyDfQkBfqEpShivu1CNgpstWrNekx0enzT8';
 
 
 //其他地方不用改。
